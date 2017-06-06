e13b3965325384cf93f443c540655a7bdbe7e3ed
Lara Kamal <lara.kamal10@gmail.com>
2017-06-06 13:12:14 -0400

    Update model usage.py

--- Rank Evaluation/model usage.py
+++ Rank Evaluation/model usage.py
@@ -14,7 +14,6 @@ json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
-# load weights into the model
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 
@@ -23,22 +22,18 @@ print("Model compiled")
 
 dataframe = pd.read_csv("labelled_queries/sea_ice.csv")
 features = dataframe.ix[:,0:10]
- 
 features = scaler.transform(features)
 
 pair_list = []
-#find the difference of every pair and append the difference to pair_list list
 for i in range(len(features)):
     for j in range(len(features)):
         final = features[i] - features[j]
         pair_list.append(final.reshape(1,-1))
   
-#initialize rank list with zeros 
 rank = []
 for i in range(len(features)):
     rank.append(0)
 
- 
 index = 0
 var = 0
 for i in range(len(pair_list)):      
@@ -46,27 +41,22 @@ for i in range(len(pair_list)):
         index = index+1
         var = 0
     else:
-        #prediction = model.predict(pair_list[i])
         class_prediction = model.predict_classes(pair_list[i])
         rank[index] = rank[index] +  class_prediction
         var = var + 1
 
 
-#sort the list and the index accordingly 
-#list that has the sorted documents 
+#sort the list and the index accordingly  
 rank_doc_list = [b[0] for b in sorted(enumerate(rank), key=lambda i:i[1], reverse = True)]
 rows = dataframe.ix[:,0:11]
 
-#create sorted rows list
 sorted_rows =[]
-#sort rows 
 for i in range(len(rank_doc_list)):
     sorted_rows.append(rows.values[rank_doc_list[i]])
     
-
 with open('results/sea_ice_sorted.csv', 'w', encoding = 'utf-8-sig') as outcsv:
     writer = csv.writer(outcsv)
     writer.writerow(['term_score', 'releaseDate_score', 'versionNum_score', 'processingL_score', 'allPop_score','monthPop_score', 'userPop_score', 'spatialR_score','temporalR_score','click_score','label'])
     for i in sorted_rows:
         writer.writerow(i)
-  
+
